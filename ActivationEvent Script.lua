ab27e610c649dafbb3332827b5b110f2c1b81987
Richy <44143554+JaydenSinespace@users.noreply.github.com>
2024-04-16 16:28:56 +0200

    Update
    Moved script from runtime box to standalone text script

---
+++ ActivationEvent Script.lua
@@ -0,0 +1,13 @@
+﻿
+watching = Space.Host.GetReference('object');
+
+function eventEnabled()
+    Space.Host.InvokeEvent('enabled')
+end
+
+function eventDisabled()
+    Space.Host.InvokeEvent('disabled')
+end
+
+watching.OnEnable(eventEnabled)
+watching.OnDisable(eventDisabled)
